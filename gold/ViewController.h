04f82024c70889741251b7bd3933743017b4de69
Yoko Yamaguchi <yokoy@yokos-macbook-air-2.local>
2016-04-30 00:13:36 -0700

    Initial Commit

---
+++ gold/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gold
+//
+//  Created by Yoko Yamaguchi on 4/30/16.
+//  Copyright © 2016 lahacks2016. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gold/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gold
+//
+//  Created by Yoko Yamaguchi on 4/30/16.
+//  Copyright © 2016 lahacks2016. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
